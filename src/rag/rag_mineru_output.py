1bb51f9e4b11faa0db80ab63243a12412e863678
jisyST <jisiyuan@sensetime.com>
2025-03-27 16:13:45 +0800

    modify env

--- src/rag/rag_mineru_output.py
+++ src/rag/rag_mineru_output.py
@@ -7,28 +7,10 @@ from lazyllm import pipeline, parallel, bind, OnlineEmbeddingModule, SentenceSpl
 from src.reader.magic_pdf_reader import MagicPDFReader
 from src.parser.magic_pdf_transform import MagicPDFTransform
 from src.rag.utils import draw_pdf_bbox
+from dotenv import load_dotenv
 
+load_dotenv()
 
-# Before running, set the environment variable:
-#
-# 1. `export LAZYLLM_GLM_API_KEY=xxxx`: the API key of Zhipu AI, default model "glm-4", `source="glm"`.
-#     You can apply for the API key at https://open.bigmodel.cn/
-#     Also supports other API keys:
-#       - LAZYLLM_OPENAI_API_KEY: the API key of OpenAI, default model "gpt-3.5-turbo", `source="openai"`.
-#           You can apply for the API key at https://openai.com/index/openai-api/
-#       - LAZYLLM_KIMI_API_KEY: the API key of Moonshot AI, default model "moonshot-v1-8k", `source="kimi"`.
-#           You can apply for the API key at https://platform.moonshot.cn/console
-#       - LAZYLLM_QWEN_API_KEY: the API key of Alibaba Cloud, default model "qwen-plus", `source="qwen"`.
-#           You can apply for the API key at https://home.console.aliyun.com/
-#       - LAZYLLM_SENSENOVA_API_KEY: the API key of SenseTime, default model "SenseChat-5", `source="sensenova"`.
-#                                  You also have to set LAZYLLM_SENSENOVA_SECRET_KEY` togather.
-#           You can apply for the API key at https://platform.sensenova.cn/home
-#     * `source` needs to be specified for multiple API keys, but it does not need to be set for a single API key.
-#
-# 2. `export LAZYLLM_DATA_PATH=path/to/docs/folder/`: The parent folder of the document folder `rag_master`.
-#                                                    Alternatively, you can set the `dataset_path` of the Document
-#                                                    to `path/to/docs/folder/rag_master` to replace
-#                                                    the setting of this environment variable.
 
 prompt = 'You will play the role of an AI question-answering assistant and complete a conversation task in which you need to provide your answer based on the given context and question. Please note that if the given context cannot answer the question, do not use your prior knowledge but tell the user that the given context cannot answer the question.'
 
